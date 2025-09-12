82e45332befc011b3bbed4043bc29b92ab271e76
Tejas-Santosh-Nalawade <tejassantoshnalawade@gmail.com>
2025-09-12 23:03:51 +0530

    Nodejs and Nginx custom Server building

---
+++ BACKEND/Nginx/SERVER/server.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+    const extName = String(path.extname(filePath)).toLowerCase();
+    const mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.svg': 'application/image/svg+xml'
+    };
+    const contentType = mimeTypes[extName] || 'application/octet-stream';
+
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            if (error.code === 'ENOENT') {
+                fs.readFile(path.join(__dirname, '404.html'), (err, data) => {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });    
+                    res.end(data, 'utf-8');
+                });
+            } else {
+                res.writeHead(500);
+                res.end(`Server Error: ${error.code}`);
+            }   
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }   
+    });
+})
+
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
